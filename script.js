c7b63f7f4011281d69723ce82be7c0d6099252b1
moltyhero <matanoded@gmail.com>
2022-06-09 04:07:53 +0300

    Calculator is basically finished without style

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let currentDisplay = "";
 let numbers = document.querySelectorAll(".number");
 let operators = document.getElementById("operators");
-operators = operators.children;
+operators = operators.childNodes;
+let operatorsArray = Array.prototype.slice.call(operators);
+operatorsArray.push(document.getElementById("result"));
+console.log('operators :>> ', operatorsArray);
 let firstNumber;
 let secondNumber;
+let currentOperation;
 let display = document.getElementById("display");
+
 
 function add(num1, num2) {
 	return num1 + num2;
@@ -31,6 +36,12 @@ function operate(num1, num2, operator) {
 		case "*":
 			return multiply(num1, num2);
 		case "/":
+			if (num2 === 0)
+			{
+				console.log("????");
+				return "What are you doing??? Diving by 0 is impossible!";
+			}
+			
 			return divide(num1, num2);
 	}
 }
@@ -43,16 +54,44 @@ function populateDisplay(num) {
 numbers.forEach(number => {
 	number.addEventListener("click", function() {
 		populateDisplay(number.innerText);
-		console.log('number :>> ', number);
 	});
 });
 
-operators.forEach(operator => {
+operatorsArray.forEach(operator => {
 	operator.addEventListener("click", function()
 	{
 		if (firstNumber)
 		{
-			secondNumber = display.innerText;
+			secondNumber = parseFloat(display.innerText);
+			let result = operate(firstNumber, secondNumber, currentOperation);
+			currentDisplay = "";
+			populateDisplay(result);
+			currentOperation = "";
+			currentDisplay = "";
+			firstNumber = "";
+			//firstNumber = result;
+			secondNumber = "";
+		}
+		else
+		{
+			if (operator.innerText.localeCompare("="))
+			{
+				firstNumber = parseFloat(display.innerText); 
+				currentOperation = operator.innerText;
+				currentDisplay = "";
+				populateDisplay(""); 
+				secondNumber = "";
+			}
+			
 		}
 	})
 })
+
+let clearBtn = document.getElementById("clear");
+clearBtn.addEventListener("click", () =>
+{
+	firstNumber = "";
+	secondNumber = "";
+	currentDisplay = "";
+	populateDisplay("");
+});
