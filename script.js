591b8ce9571e814d3e2d56a8f11257a44caabc50
moltyhero <matanoded@gmail.com>
2022-06-09 03:14:35 +0300

    Make the display show numbers

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let currentDisplay = "";
+let numbers = document.querySelectorAll(".number");
+let operators = document.getElementById("operators");
+operators = operators.children;
+let firstNumber;
+let secondNumber;
+let display = document.getElementById("display");
+
 function add(num1, num2) {
 	return num1 + num2;
 }
@@ -26,3 +34,25 @@ function operate(num1, num2, operator) {
 			return divide(num1, num2);
 	}
 }
+
+function populateDisplay(num) {
+	currentDisplay += num;
+	display.innerText = currentDisplay;
+}
+
+numbers.forEach(number => {
+	number.addEventListener("click", function() {
+		populateDisplay(number.innerText);
+		console.log('number :>> ', number);
+	});
+});
+
+operators.forEach(operator => {
+	operator.addEventListener("click", function()
+	{
+		if (firstNumber)
+		{
+			secondNumber = display.innerText;
+		}
+	})
+})
